dfa77a81e5c739a0b2580aeb5a89f14aad374e51
eoindn <eoin.donnelly03@gmail.com>
2024-11-05 22:47:46 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from car import Car
+
+car1 = Car("SF90 XX", 2024, "red", False,"£500,000")
+car2 = Car("Lamborghini", 2014, "blue", True,"£4,000,000")
+car3 = Car("Bugatti Chiron", 2020, "black", True,"£3,000,000")
+
+# def car3_info():
+#     print(car3.model)
+#     print(car3.price)
+#     print(car3.year)
+#     print(car3.color)
+#     print(car3.for_sale)
+
+car1.describe()
+
+
+
